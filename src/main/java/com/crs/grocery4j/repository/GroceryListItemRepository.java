f7d7de758f7557d03ef2e22320bb685811149d8f
Christopher Sipes <crs@Christophers-MacBook-Pro.local>
2018-09-03 22:43:40 -0400

    Added repository and service layer for grocery list items

---
+++ src/main/java/com/crs/grocery4j/repository/GroceryListItemRepository.java
@@ -0,0 +1,15 @@
+package com.crs.grocery4j.repository;
+
+import com.crs.grocery4j.domain.database.GroceryListItem;
+import com.crs.grocery4j.domain.database.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the User entity.
+ */
+@Repository
+public interface GroceryListItemRepository extends JpaRepository<GroceryListItem, Long>, JpaSpecificationExecutor<GroceryListItem> {
+
+}
---
+++ src/main/java/com/crs/grocery4j/service/domain/GroceryItemService.java
@@ -0,0 +1,68 @@
+package com.crs.grocery4j.service.domain;
+
+import com.crs.grocery4j.domain.database.GroceryListItem;
+import com.crs.grocery4j.domain.database.Item;
+import com.crs.grocery4j.repository.GroceryListItemRepository;
+import com.crs.grocery4j.repository.ItemRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Created by crs on 9/3/18.
+ */
+@Service
+public class GroceryItemService implements RestService<GroceryListItem> {
+
+    @Inject
+    private GroceryListItemRepository groceryListItemRepository;
+
+    @Override
+    public Long count(Specification<GroceryListItem> specification) {
+        return this.groceryListItemRepository.count();
+    }
+
+    @Override
+    public GroceryListItem findById(Long objectId) {
+        return this.groceryListItemRepository.findOne(objectId);
+    }
+
+    @Override
+    public Page<GroceryListItem> findAll(Specification<GroceryListItem> specification, Pageable pageRequest) {
+        return this.groceryListItemRepository.findAll(specification, pageRequest);
+    }
+
+    @Override
+    public List<GroceryListItem> findAll(Specification<GroceryListItem> specification, Sort sort) {
+        return this.groceryListItemRepository.findAll(specification, sort);
+    }
+
+    @Override
+    public GroceryListItem create(GroceryListItem entity) {
+        return this.groceryListItemRepository.save(entity);
+    }
+
+    @Override
+    public GroceryListItem update(GroceryListItem entity) {
+
+        if (entity.getId() == null) {
+            return null;
+        }
+
+        return this.groceryListItemRepository.save(entity);
+    }
+
+    @Override
+    public void delete(GroceryListItem entity) {
+        if (entity.getId() == null) {
+            return;
+        }
+
+        this.groceryListItemRepository.delete(entity.getId());
+    }
+}
